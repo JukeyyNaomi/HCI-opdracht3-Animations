4c275ac5aa79a682dd4706c6cba20c16c3aa4fa7
JukeyyNaomi <naomi.beekhuizen@gmail.com>
2021-05-19 17:18:35 +0200

    kjnjk

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-
+// equivalent to 60 fps
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 1;
+var rotationSpeed = 0.1;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -9,5 +9,20 @@ function spin(){
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, 16); 
-equivalent to 60 fps
+ setInterval(spin, 16); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
